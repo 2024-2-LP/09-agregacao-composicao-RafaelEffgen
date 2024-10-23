b57b3851e6da684e8781684982014d201c68384c
RafaelEffgen <rafael.effgen@sptech.school>
2024-10-23 19:41:40 -0300

    Exercícios finalizados

--- src/main/java/school/sptech/Avaliacao.java
+++ src/main/java/school/sptech/Avaliacao.java
@@ -1,5 +1,38 @@
 package school.sptech;
 
 public class Avaliacao {
+    private String descricao;
+    private Double qtdEstrelas;
 
+    @Override
+    public String toString() {
+        return """
+                A descricao do livro é: %s
+                e a quantidade de estrelas é: %.2f
+                """.formatted(descricao, qtdEstrelas);
+    }
+
+    public Avaliacao() {
+    }
+
+    public Avaliacao(String descricao, Double qtdEstrelas) {
+        this.descricao = descricao;
+        this.qtdEstrelas = qtdEstrelas;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public Double getQtdEstrelas() {
+        return qtdEstrelas;
+    }
+
+    public void setQtdEstrelas(Double qtdEstrelas) {
+        this.qtdEstrelas = qtdEstrelas;
+    }
 }
--- src/main/java/school/sptech/Biblioteca.java
+++ src/main/java/school/sptech/Biblioteca.java
@@ -1,5 +1,70 @@
 package school.sptech;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Biblioteca {
+    private String nome;
+    private List<Livro> livros;
+
+    public Biblioteca(String nome, List<Livro> livros) {
+        this.nome = nome;
+        this.livros = new ArrayList<>();
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public void adicionarLivro(Livro livro) {
+        if (livro != null) {
+            if (livro.getTitulo() != null && !livro.getTitulo().isBlank()) {
+                if (livro.getAutor() != null && !livro.getAutor().isBlank()) {
+                    if (livro.getDataPublicacao() != null) {
+                        livros.add(livro);
+                    }
+                }
+            }
+        }
+    }
+
+    public void removerLivroPorTitulo(String titulo){
+        for (int i = 0; i < livros.size(); i++) {
+            if (titulo.equalsIgnoreCase(livros.get(i).getTitulo())){
+                livros.remove(i);
+            }
+        }
+    }
+
+    public Livro buscarLivroPorTitulo(String titulo){
+        if(titulo != null && !titulo.isBlank()){
+            for (Livro livroAtual : livros){
+                if(livroAtual.getTitulo().equalsIgnoreCase(titulo)){
+                    return livroAtual;
+                }
+            }
+        }
+        return null;
+    }
+
+    public Integer contarLivros(){
+        return livros.size();
+    }
+
+    public List<Livro> obterLivrosAteAno(Integer ano) {
+        List<Livro> livrosAteAno = new ArrayList<>();
+        for (Livro livro : livros) {
+            if (livro.getDataPublicacao().getYear() <= ano) {
+                livrosAteAno.add(livro);
+            }
+        }
+
+        return livrosAteAno;
+    }
 
 }
